b9cc40329056f810638a8a5b095b6477226be4f0
I-stack-glitch <ff2974247@gmail.com>
2025-09-21 00:36:12 +0400

    Deleted Helmet

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: true,
     credentials: true
 }))
 app.get("/", (req, res)=> {
