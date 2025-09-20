f51be59cafbc6fa5c306b722fb222d0f0add1229
I-stack-glitch <ff2974247@gmail.com>
2025-09-21 00:39:53 +0400

    Updated Cors for Frontend

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cors({
-    origin: true,
+    origin: "https://clariospace-frontend.onrender.com",
     credentials: true
 }))
 app.get("/", (req, res)=> {
