8fd71a59329892b8a92e7488b99ead8d9c0d7a3e
I-stack-glitch <ff2974247@gmail.com>
2025-09-21 01:14:56 +0400

    Updated Only Multer

--- Backend/multer.config.js
+++ Backend/multer.config.js
@@ -3,15 +3,29 @@ const multer = require("multer")
 const path = require("path")
 
 //! Building Multer Storage Configuration
+// const userFileStorage = multer.diskStorage({
+//     destination: function(req , file , cb){
+//         // cb(null , "../userUploads/") //! --> Recent
+//          cb(null, path.join(__dirname, "../userUploads"))
+//     }, filename: function(req , file , cb) {
+//         const uniqueName = Date.now() + "-" + file.originalname
+//         cb(null, uniqueName)
+//     }
+// })
+
+const uploadPath = path.resolve(__dirname, "../userUploads");
+if (!fs.existsSync(uploadPath)) fs.mkdirSync(uploadPath, { recursive: true });
+
 const userFileStorage = multer.diskStorage({
-    destination: function(req , file , cb){
-        // cb(null , "../userUploads/") //! --> Recent
-         cb(null, path.join(__dirname, "../userUploads"))
-    }, filename: function(req , file , cb) {
-        const uniqueName = Date.now() + "-" + file.originalname
-        cb(null, uniqueName)
+    destination: function(req, file, cb){
+        cb(null, uploadPath);
+    },
+    filename: function(req, file, cb){
+        const uniqueName = Date.now() + "-" + file.originalname;
+        cb(null, uniqueName);
     }
-})
+});
+
 
 //! Building Multer Instance
 const userUploads = multer ({storage: userFileStorage})
